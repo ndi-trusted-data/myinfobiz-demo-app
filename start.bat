510872d5683d280248e1c2532444c2fd1165982f
Toh Sin Gui <sin.gui.toh@accenture.com>
2020-01-09 16:07:05 +0800

    update staging cert name

--- start.bat
+++ start.bat
@@ -1,6 +1,6 @@
 @ECHO off
-set DEMO_APP_SIGNATURE_CERT_PRIVATE_KEY=./ssl/demoapp-client-privatekey-2018.pem
-set MYINFO_CONSENTPLATFORM_SIGNATURE_CERT_PUBLIC_CERT=./ssl/staging_myinfo_public_cert.pem
+set DEMO_APP_SIGNATURE_CERT_PRIVATE_KEY=./ssl/stg-demoapp-client-privatekey-2018.pem
+set MYINFO_CONSENTPLATFORM_SIGNATURE_CERT_PUBLIC_CERT=./ssl/staging_myinfo_public_cert.cer
 
 set MYINFO_APP_CLIENT_ID=STG2-MYINFO-SELF-TEST
 set MYINFO_APP_CLIENT_SECRET=44d953c796cccebcec9bdc826852857ab412fbe2
